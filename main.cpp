21c36d63f65811f8e5dbd1ed7a0f9fcd01bcda36
Hoi Cheong Kwan <hkwan003@ucr.edu>
2015-04-14 02:36:22 -0700

    push to my repo

--- main.cpp
+++ main.cpp
@@ -1,22 +1,35 @@
 #include <iostream>
 #include <string>
+#include <stdio.h>
 #include <vector>
+#include <stdlib.h>   
+#include <string.h>
+#include <boost/tokenizer.hpp>
+#include <unistd.h>
 
+using namespace boost;
 using namespace std;
+
 
 int main()
 {
-	string command;
-	cout << "$ ";
-	getline(cin,command);
-	cout << command << endl;
-	vector<char > tokenizer(2*command.size());
-	for(int x = 0; x < command.size(); x++)
-	{
-		tokenizer[x] = command.at(x);
-	}
-    cout << "output size of string: " << command.size() << endl;
-    cout << "output size of vector: " << tokenizer.size() << endl;
+    vector<string> command;
+    cout  << "$ ";
+    string converter;                           //converts all bits of string into one piece
+    string to_be_tokenized;
+    getline(cin, to_be_tokenized);
+    char_separator<char> delim("^&| ", ";", keep_empty_tokens);
+    tokenizer< char_separator<char>  > mytok(to_be_tokenized, delim); 
+ 
+    for(auto it = mytok.begin(); it != mytok.end(); ++it)
+    {
+        converter += " ";         
+        converter += *it;
+        converter += " \0";
+    }
+    cout << converter << endl;
+    command.push_back(converter);
+    
 
 
 
@@ -24,12 +37,25 @@ int main()
 
 
 
+}
+
+/* || && ;
+
+succeeds ||
+notsucceed &&
+
+ls -a
+
+[ls] [-a]
+
+execvp([ls],[[ls],[-a]])
+char* argumentList[50000];
+char executable[50000];
+argumentList[0] = executable;
+unsigned int size = 0;
+ls -a \0
+execvp(argumentList[0], argumentList);
+*/
 
 
-
-
-
-
-
-
-}	
+    
