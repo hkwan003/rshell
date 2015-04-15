8d6a7965ec955538fa45c6afbf36445a698d1b23
Hoi Cheong Kwan <hkwan003@ucr.edu>
2015-04-15 02:01:34 -0700

    fixed connectors, works not in vectors

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ using namespace std;
 
 
 
-
+//Global Variable vectors
 vector<int> connect_Pos; //holds the positions of possible connectors in prompt
 vector<int> kinds_of_connect; //holds what kind of connector are in prompt
 
@@ -27,21 +27,26 @@ for(int x = 0; x < size; x++)
         {
             connect_Pos.push_back(x);
             kinds_of_connect.push_back(0);
-         }
-        else if(prompt[x] ==  '|' && prompt[x + 1] == '|') 
-        {
-            connect_Pos.push_back(x);
-            kinds_of_connect.push_back(1);
-            kinds_of_connect.push_back(1);
         }
-        else if(prompt[x] == '&' && prompt[x + 1] == '&')
+        else if(prompt[x] ==  '|' && (x + 1 < size &&  prompt[x + 1] == '|')) 
+        {
+            connect_Pos.push_back(x);
+            kinds_of_connect.push_back(1);
+            kinds_of_connect.push_back(1);
+            x++;
+        }
+        else if(prompt[x] == '&' && (x + 1 < size && prompt[x + 1] == '&'))
         {
             connect_Pos.push_back(x);
             kinds_of_connect.push_back(2);
             kinds_of_connect.push_back(2);
+            x++;
 
         }
-        kinds_of_connect.push_back(-1);        
+        else
+        {
+            kinds_of_connect.push_back(-1);        
+        }
     }
 }
 
@@ -63,7 +68,7 @@ int main(int argc, char **argv)
     }
     prompt_holder[to_be_tokenized.size()] = '\0';
 
-    possible_connector(to_be_tokenized ,to_be_tokenized.size() + 1);
+    possible_connector(to_be_tokenized ,to_be_tokenized.size());
 
     int i = fork();
     if(i == 0)
@@ -78,9 +83,15 @@ int main(int argc, char **argv)
         cout << "hello world" << endl;
     }
 
+    for(int x = 0; x < connect_Pos.size() ; x++)
+    {
+        cout << "Connect_pos: " << connect_Pos.at(x) << endl;
+    }
 
-
-
+    for(int x = 0; x < kinds_of_connect.size() ; x++)
+    {
+        cout << "kinds of connect: " << kinds_of_connect.at(x) << endl;
+    }
 
 
 
