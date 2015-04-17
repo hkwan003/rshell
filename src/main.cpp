47551551d9a1cb85c1374c59d7585a7892b9f4e9
Hoi Cheong Kwan <hkwan003@ucr.edu>
2015-04-16 20:48:33 -0700

    execvp works with passed in arguments

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 #include <list>
 #include <string.h>
 #include <vector>
-
+#include <cstdlib>
 
 using namespace std;
 
@@ -18,6 +18,11 @@ using namespace std;
 //Global Variable vectors
 vector<int> connect_Pos; //holds the positions of possible connectors in prompt
 vector<int> kinds_of_connect; //holds what kind of connector are in prompt
+
+
+
+
+
 
 
 void possible_connector(string prompt,int size)
@@ -55,14 +60,31 @@ for(int x = 0; x < size; x++)
 
 int main(int argc, char **argv)
 {
-    
+    char* host = (char*)malloc(500);
+    string userinfo;
 
-    vector <string> Vctr_of_commands;
+    //error checks the user
+    if(getlogin() != NULL)
+    {
+        userinfo = getlogin();
+    }
+    else
+    {
+        perror("error getting user info");
+    }
+
+    if(gethostname(host, 500) == -1)
+    {
+        perror("Error getting host name");
+    }
+    //outputs the userinfo with login and host 
+    cout << userinfo << "@" << host << "$";
+    //////////////////////////////////////////////login part done, next is all shell commands 
     char prompt_holder[50000];//orginal array to hold prompt
     char *token;//used to break up using string tokenizer
+    char *comd_arr[50000];
+    unsigned int comd_arr_cnt = 0;
 
-
-    cout  << "$ ";
     string converter;                           //converts all bits of string into one piece
     string to_be_tokenized;
     getline(cin, to_be_tokenized);
@@ -71,32 +93,28 @@ int main(int argc, char **argv)
         prompt_holder[x] =  to_be_tokenized.at(x);
     }
     prompt_holder[to_be_tokenized.size()] = '\0';
-
-    possible_connector(to_be_tokenized ,to_be_tokenized.size());
+    possible_connector(to_be_tokenized ,to_be_tokenized.size());//function to see pos of connectors
 
     token = (prompt_holder);
-    cout << "output what is suppose to be tokenized" << endl;
     while(token != NULL)
     {
-        cout << token << endl;
-        char foo[10000] = {*token};
-        string s(foo);
-        cout << "output strings: " << s << endl;
-        Vctr_of_commands.push_back(s);
-        cout<< "What is suppose to output: "  << Vctr_of_commands[0] << endl;
-        token = strtok(NULL, "|;&");
+        cout << *token << endl;
+        comd_arr[comd_arr_cnt] = token;
+        comd_arr_cnt++;
+        token = strtok(NULL, "|; &");
     }
-
-
-
-    string finished_tok = strtok(prompt_holder, " &|;");
+    
+    for(unsigned int x  = 0; x < comd_arr_cnt; x++)
+    {
+        cout << comd_arr[x] << " "; 
+    }
     
     int i = fork();
     if(i == 0)
     {
-        if(-1 == execvp(prompt_holder, argv))
+        if(-1 == execvp(comd_arr[0], comd_arr))
         {
-            perror("execvp");
+            perror("There is a problem with the prompt entered");
         }
     }
     else
@@ -104,17 +122,7 @@ int main(int argc, char **argv)
         cout << "hello world" << endl;
     }
 
-    for(unsigned int x = 0; x < connect_Pos.size() ; x++)
-    {
-        cout << "Connect_pos: " << connect_Pos.at(x) << endl;
-    }
-
-    for(unsigned int x = 0; x < kinds_of_connect.size() ; x++)
-    {
-        cout << "kinds of connect: " << kinds_of_connect.at(x) << endl;
-    }
-
-
+      
 
 }
 
