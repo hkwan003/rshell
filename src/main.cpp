092553100a9e0ae4e28f138312ee8742e9773ad5
Hoi Cheong Kwan <hkwan003@ucr.edu>
2015-04-18 01:55:00 -0700

    redoing algorithm style, big changes

--- src/main.cpp
+++ src/main.cpp
@@ -8,60 +8,64 @@
 #include <string.h>
 #include <vector>
 #include <cstdlib>
+#include <sys/wait.h>
+
+
+
 
 using namespace std;
 
-
-
-
-
-//Global Variable vectors
-vector<int> connect_Pos; //holds the positions of possible connectors in prompt
-vector<int> kinds_of_connect; //holds what kind of connector are in prompt
-
-
-
-
-
-
-
-void possible_connector(string prompt,int size)
+void fixing_spacing_command(char *org_prompt)
 {
-for(int x = 0; x < size; x++)
+    char *finished_prompt[50000];
+    //x is passed in prompt
+    //i is finished prompt after changing spaces
+    for(int x = 0, int i = 0; org_prompt[x] != '\0'; x++, i++)
     {
-        if(prompt.at(x)  == ';')
+        if(org_prompt[x] == '#')
         {
-            connect_Pos.push_back(x);
-            kinds_of_connect.push_back(0);
+            org_prompt[x] = '\0';
+            finished_prompt[i] = '\0';
         }
-        else if(prompt[x] ==  '|' && (x + 1 < size &&  prompt[x + 1] == '|')) 
+        else if(org_prompt[x] == ';')
         {
-            connect_Pos.push_back(x);
-            kinds_of_connect.push_back(1);
-            kinds_of_connect.push_back(1);
-            x++;
+            finished_prompt[i] = ' ';
+            finished_prompt[++i] = ';';
+            finished_prompt[++i] = ' ';
         }
-        else if(prompt[x] == '&' && (x + 1 < size && prompt[x + 1] == '&'))
+        else if(org_prompt[x] == '&' && org_prompt[x] == '&')
         {
-            connect_Pos.push_back(x);
-            kinds_of_connect.push_back(2);
-            kinds_of_connect.push_back(2);
-            x++;
-
+            finished_prompt[i] = ' ';
+            finished_prompt[++i] = '&';
+            finished_prompt[++i] = '&';
+            finished_prompt[++i] = ' ';
+            ++i;
+        }
+        else if(org_prompt[x] == '|' && org_prompt[x] == '|')
+        {
+            finished_prompt[i] = ' ';
+            finished_prompt[++i] = '|';
+            finished_prompt[++i] = '|';
+            finished_prompt[++i] = ' ';
+            ++i;
         }
         else
         {
-            kinds_of_connect.push_back(-1);        
+            finished_prompt[i] = org_prompt[x];
         }
+        if(org_prompt[x + 1] == '\0') finished_prompt[i + 1] = '\0';
     }
-}
+    strcpy(org_prompt, finished_prompt);
 
 
 
 int main(int argc, char **argv)
 {
+    int status;
     char* host = (char*)malloc(500);
     string userinfo;
+    bool prompter = true;
+    char* token;            //will be bit of code strtok cuts off
 
     //error checks the user
     if(getlogin() != NULL)
@@ -77,52 +81,84 @@ int main(int argc, char **argv)
     {
         perror("Error getting host name");
     }
-    //outputs the userinfo with login and host 
-    cout << userinfo << "@" << host << "$";
-    //////////////////////////////////////////////login part done, next is all shell commands 
-    char prompt_holder[50000];//orginal array to hold prompt
-    char *token;//used to break up using string tokenizer
-    char *comd_arr[50000];
-    unsigned int comd_arr_cnt = 0;
+    //outputs the userinfo with login and host
+    while(prompter)
+    {
+        cout << userinfo << "@" << host << " $ ";
+        //////////////////////////////////////////////login part done, next is all shell commands 
+        int connect_check = ;           //holds to see what kind of connectors I have 
+        char prompt_holder[50000];//orginal array to hold prompt
+        char *comd_arr[50000];
+        unsigned int comd_arr_cnt = 0;
 
-    string converter;                           //converts all bits of string into one piece
-    string to_be_tokenized;
-    getline(cin, to_be_tokenized);
-    for(unsigned int x = 0; x < to_be_tokenized.size(); x++)
-    {
-        prompt_holder[x] =  to_be_tokenized.at(x);
-    }
-    prompt_holder[to_be_tokenized.size()] = '\0';
-    possible_connector(to_be_tokenized ,to_be_tokenized.size());//function to see pos of connectors
-
-    token = (prompt_holder);
-    while(token != NULL)
-    {
-        cout << *token << endl;
-        comd_arr[comd_arr_cnt] = token;
-        comd_arr_cnt++;
-        token = strtok(NULL, "|; &");
-    }
-    
-    for(unsigned int x  = 0; x < comd_arr_cnt; x++)
-    {
-        cout << comd_arr[x] << " "; 
-    }
-    
-    int i = fork();
-    if(i == 0)
-    {
-        if(-1 == execvp(comd_arr[0], comd_arr))
+        string converter;                           //converts all bits of string into one piece
+        string to_be_tokenized;
+        getline(cin, to_be_tokenized);
+        for(unsigned int x = 0; x < to_be_tokenized.size(); x++)
         {
-            perror("There is a problem with the prompt entered");
+            prompt_holder[x] =  to_be_tokenized.at(x);
         }
-    }
-    else
-    {
-        cout << "hello world" << endl;
-    }
+        prompt_holder[to_be_tokenized.size()] = '\0';
+        fixed_spacing_command(prompt_holder);
+        token = strtok(prompt_holder, "\t ";
+        while(token != NULL && prompter)
+        {
+            if(!strcmp(token, ";")) connect_check = 0;
+            else if(!strcmp(token, "||")) connect_check = 1;
+            else if(!strcmp(token, "&&")) connect_check = 2;
+            connect_check = -1;
 
+            if(connect_check == -1)
+            {
+                comd_arr[comd_arr_cnt] = token;
+                comd_arr_cnt++;
+            }
+
+        }
+
+        
       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+        
+        int i = fork();
+        if(i <= -1)     //this would be an error
+        {
+            perror("an error occured with fork()");
+            exit(1);
+        }
+        else if(i == 0)
+        {
+            if(-1 == execvp(comd_arr[0], comd_arr))
+            {
+                perror("There is a problem with the prompt entered");
+            }
+        }
+        else if(i > 0)          //parent process
+        {
+            if(waitpid(i,&status, 0) == -1)
+            {
+                perror("Error with waitpid");
+            }
+
+        }
+
+    }  
 
 }
 
